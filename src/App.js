aa66cb05317dec0e2e56e9819b409a6dc958de98
Dean Beebe <beebe.d@northeastern.edu>
2022-01-21 17:56:28 -0500

    start of sherpa

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { CgYinyang } from "react-icons/cg";
+import Compensation from "./components/Compensation";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CgYinyang className={"text-white align-center"} />
+        <p>
+          Sherpa
+        </p>
+      </header>
+
+
+    </div>
+  );
+}
+
+export default App;
